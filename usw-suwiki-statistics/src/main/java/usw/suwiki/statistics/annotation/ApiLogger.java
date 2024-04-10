b6a14d0ac8fcd637783618933a34aa20b619284c
Diger <kitten.diger@gmail.com>
2024-04-10 14:19:47 +0900

    refactor: annotation profile scope

--- usw-suwiki-statistics/src/main/java/usw/suwiki/statistics/annotation/ApiLogger.java
+++ usw-suwiki-statistics/src/main/java/usw/suwiki/statistics/annotation/ApiLogger.java
@@ -4,9 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.context.annotation.Profile;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Profile({"prod", "dev", "local"})
 public @interface ApiLogger {
     String option();
 }
--- usw-suwiki-statistics/src/main/java/usw/suwiki/statistics/annotation/CacheStatics.java
+++ usw-suwiki-statistics/src/main/java/usw/suwiki/statistics/annotation/CacheStatics.java
@@ -4,8 +4,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.context.annotation.Profile;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Profile({"prod", "dev", "local"})
 public @interface CacheStatics {
 }

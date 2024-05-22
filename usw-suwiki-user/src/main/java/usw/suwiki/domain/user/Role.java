324d7c9c106d6a7bd55348eb5226e43066ef6745
Hejow <gmlwh124@naver.com>
2024-05-22 16:33:48 +0900

    refactor: remove unnecessary field on user role

--- usw-suwiki-user/src/main/java/usw/suwiki/domain/user/Role.java
+++ usw-suwiki-user/src/main/java/usw/suwiki/domain/user/Role.java
@@ -1,23 +1,15 @@
 package usw.suwiki.domain.user;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
-
-  ADMIN("ADMIN", "관리자 권한"),
-  USER("USER", "사용자 권한");
-
-  private final String key;
-  private final String title;
+  ADMIN,
+  USER,
+  ;
 
   public boolean isAdmin() {
     return this.equals(ADMIN);
   }
 
   public boolean isAdmin(String key) {
-    return ADMIN.key.equals(key.toUpperCase());
+    return ADMIN.name().equals(key.toUpperCase());
   }
 }

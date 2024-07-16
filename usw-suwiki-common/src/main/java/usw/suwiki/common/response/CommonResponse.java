7ddca1ce70537e5803dfba0dbca5144ffbeecde0
wlsdlf2 <wlsdlfzg@naver.com>
2024-07-16 14:41:53 +0900

    refactor: add success field

--- usw-suwiki-common/src/main/java/usw/suwiki/common/response/CommonResponse.java
+++ usw-suwiki-common/src/main/java/usw/suwiki/common/response/CommonResponse.java
@@ -1,20 +1,21 @@
 package usw.suwiki.common.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Map;
-
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommonResponse<T> {
     private final T data;
+    private final boolean success;
 
     public static <T> CommonResponse<T> ok(T data) {
-        return new CommonResponse<>(data);
+        return new CommonResponse<>(data, true);
     }
 
-    public static CommonResponse<?> success() { return new CommonResponse<>(Map.of("success", true)); }
+    public static CommonResponse<?> success() { return new CommonResponse<>(null,true); }
 }
 
